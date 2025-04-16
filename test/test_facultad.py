d80b04f79f46a109c0ae7e4f7c9b461296b403ab
Agustin <agustincampos04@gmail.com>
2025-04-16 19:18:52 -0300

    primer commit

---
+++ test/test_facultad.py
@@ -0,0 +1,19 @@
+import unittest
+from flask import current_app
+from app import create_app
+import os
+
+class FacultadsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        os.environ['FLASK_CONTEXT'] = 'testing'
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_facultad_creation(self):
+        facultad = Facultad()
+        self.assertNot(facultad)
